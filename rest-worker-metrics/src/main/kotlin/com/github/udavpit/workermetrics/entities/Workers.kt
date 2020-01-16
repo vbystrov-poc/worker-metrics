11b0a65bb6f673bc153a05e54e284f47d0132f53
Vladimir Bystrov <vladimir.bystrov@pluto.tv>
2020-01-16 14:32:50 +0300

    remove spring-boot

--- rest-worker-metrics/src/main/kotlin/com/github/udavpit/workermetrics/entities/Workers.kt
+++
@@ -1,7 +0,0 @@
-package com.github.udavpit.workermetrics.entities
-
-import io.quarkus.hibernate.orm.panache.PanacheEntity
-import javax.persistence.Entity
-
-@Entity
-data class Worker(val name: String, val host: String, val port: Int) : PanacheEntity()
--- rest-worker-metrics/src/test/kotlin/com/github/udavpit/workermetrics/resources/NativeWorkerResourceIT.kt
+++
@@ -1,6 +0,0 @@
-package com.github.udavpit.workermetrics.resources
-
-import io.quarkus.test.junit.NativeImageTest
-
-@NativeImageTest
-open class NativeWorkerResourceIT : WorkerResourceTest()
